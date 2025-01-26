82f87e8b1d2ed7528d958b693ee41a847355764e
mahsanajafi <mahsa.najafi2006@gmail.com>
2025-01-26 12:37:09 +0330

    projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,19 @@ import Project from "./components/project/Project";
 import About from "./components/about/About";
 
 export default function Home() {
+  const projectList=[
+    {title:"E-Commerce Project",des:"E-commerce store project using React and TypeScript. Utilizes Axios for API calls, Zustand for state management, and Tailwind CSS for styling. React Hook Form is implemented for efficient form handling, creating a robust and user-friendly online shopping experience.",src:"/images/e-commerce.png"}
+  ]
   return (
     <main className="items-start justify-start gap-24 flex flex-col p-8 pb-20">
       <About/>
       <Skills/>
-      <Project/>
+      {
+        projectList.map((project,index)=>(
+
+          <Project project={project} key={index}/>
+        ))
+      }
     </main>
   );
 }
